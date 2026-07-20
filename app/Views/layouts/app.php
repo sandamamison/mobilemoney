5564b3da1404d1249f825d00810ec3e166c1d988
Sanda <sandamamison1@gmail.com>
2026-07-20 14:40:52 +0300

    modif app.php

--- app/Views/layouts/app.php
+++ app/Views/layouts/app.php
@@ -10,6 +10,7 @@
     
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets/css/mobilemoney.css?v=1') ?>">
     
     <style>
         :root {
