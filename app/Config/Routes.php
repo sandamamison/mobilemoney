d31315880f988f54c4c4f7784e6175253ac8dad5
Sanda <sandamamison1@gmail.com>
2026-07-20 11:10:19 +0300

    [feat] bareme

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -17,3 +17,12 @@ $routes->post('/typesoperation/store', 'TypeOperationController::store');
 $routes->post('/typesoperation/delete', 'TypeOperationController::delete');
 $routes->get('/typesoperation/edit/(:num)', 'TypeOperationController::edit/$1');
 $routes->post('/typesoperation/update/(:num)', 'TypeOperationController::update/$1');
+
+$routes->get('/bareme', 'BaremeController::index');
+$routes->get('/bareme/create', 'BaremeController::create');
+$routes->post('/bareme/store', 'BaremeController::store');
+$routes->get('/bareme/edit/(:num)', 'BaremeController::edit/$1');
+$routes->post('/bareme/update/(:num)', 'BaremeController::update/$1');
+$routes->post('/bareme/delete', 'BaremeController::delete');
+$routes->match(['get', 'post'], '/bareme/test', 'BaremeController::testFrais');
+$routes->get('/bareme/api/frais', 'BaremeController::apiFrais');
--- app/Models/BaremeFraisModel.php
+++ app/Models/BaremeFraisModel.php
@@ -19,4 +19,16 @@ class BaremeFraisModel extends Model
 
     protected array $casts = [];
     protected array $castHandlers = [];
+
+    public function getFrais(string $typeCode, int $montant): int
+    {
+        $sql = "SELECT bf.frais FROM baremes_frais bf
+                JOIN types_operations t ON t.id = bf.type_operation_id
+                WHERE t.code = ? AND bf.montant_min <= ? AND bf.montant_max >= ? AND bf.actif = 1
+                LIMIT 1";
+
+        $row = $this->db->query($sql, [$typeCode, $montant, $montant])->getRow();
+
+        return $row ? (int) $row->frais : 0;
+    }
 }
